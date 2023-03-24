76e27e360f0483d9aa41827bfc4964f3cb4f8f3e
dushuais <1137896420@qq.com>
2023-03-24 18:31:30 +0800

    添加.vscode文件和enum

--- src/enum/app.ts
+++ src/enum/app.ts
@@ -2,10 +2,11 @@
  * @Author: dushuai
  * @Date: 2023-03-19 19:00:26
  * @LastEditors: dushuai
- * @LastEditTime: 2023-03-24 18:18:04
+ * @LastEditTime: 2023-03-24 18:30:49
  * @Description: 枚举文件
  */
 
 export enum Router {
-  首页 = '/'
+  首页 = '/',
+  游戏页 = '/game'
 }
